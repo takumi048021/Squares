59ada8a1346199e632d399a1fcd5c97548629703
50616014 <50616014@50616014-pc>
2017-11-20 17:36:35 +0900

    傾き:8.714286
    切片:2.071429

---
+++ src/squares/Squares_main.java
@@ -0,0 +1,22 @@
+package squares;
+
+public class Squares_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x[] = { 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2 };
+		double y[] = { 2.0, 3.9, 5.3, 7.3, 9.7, 10.5, 12.4 };
+		double theta[] = new double[2];
+
+		Squares_lib slib = new Squares_lib(x, y);
+		for(int i = 0; i < 100; i++){
+	      theta = slib.getDx();
+		  System.out.println(i+" : theta[0] = "+theta[0]);
+		  System.out.println(i+" : theta[1] = "+theta[1]);
+		  System.out.println(i+" : Objective function = "+slib.getObject());
+		}
+
+	}
+
+}
